c34bba345bb0d2237e41b1c24dc2433a9ea9031b
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-06-25 23:51:49 +0200

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+	constructor(private readonly authService: AuthService) {}
+
+	@UseGuards(LocalAuthGuard)
+	@Post('login')
+	login(@Request() req): any {
+		return this.authService.login(req.user);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('protected')
+	getHello(@Request() req): string {
+		return req.user;
+	}
+}
